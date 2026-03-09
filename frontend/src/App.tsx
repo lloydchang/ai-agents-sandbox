576290bb4672f5d1c1a45899b4b213e987f91454
lloydchang <lloydchang@gmail.com>
2026-03-09 01:37:12 -0700

    Fix react-use version and revert to basic React app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { FlatRoutes } from '@backstage/core-app-api';
 
 const app = (
-  <FlatRoutes>
-    <div>
-      <h1>Welcome to Backstage + Temporal Sandbox</h1>
-      <p>Backstage is now working!</p>
-    </div>
-  </FlatRoutes>
+  <div>
+    <h1>Welcome to Backstage + Temporal Sandbox</h1>
+    <p>Basic React app working - Backstage integration next step</p>
+  </div>
 );
 
 export default app;
