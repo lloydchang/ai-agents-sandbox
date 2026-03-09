ddf1e8e12d730f26810d50b4222cd77f83b97c18
lloydchang <lloydchang@gmail.com>
2026-03-08 18:36:21 -0700

    Create index.ts entry point that imports index.tsx

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,14 +1,1 @@
-import './index.css';
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import app from './App';
-
-const rootElement = document.getElementById('root') as HTMLElement;
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    {app}
-  </React.StrictMode>,
-);
+import './index.tsx';
