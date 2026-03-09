513c377351394c30b8585a01e5407afcb2576a48
lloydchang <lloydchang@gmail.com>
2026-03-08 19:13:21 -0700

    Simplify App.tsx to basic React div for testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import { FlatRoutes } from '@backstage/core-app-api';
-
-import Root from './components/Root';
 
 const app = (
-  <Root>
-    <FlatRoutes>
-      <Route path="/" element={<div><h1>Welcome to Backstage + Temporal Sandbox</h1></div>} />
-    </FlatRoutes>
-  </Root>
+  <div>
+    <h1>Welcome to Backstage + Temporal Sandbox</h1>
+    <p>Basic rendering test</p>
+  </div>
 );
 
 export default app;
